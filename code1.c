1aeb5e1519dffbaf2aa5fd63b1aedb3e822cffde
beckmalinowski <beckmalinowski@gmail.com>
2024-10-03 12:00:04 -0500

    wrote thorough comments

--- code1.c
+++ code1.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
 int main() {
+    // the values of each possible point source are set as constants here.
     const int TD_2PC = 8;
     const int TD_1PC = 7;
     const int TD = 6;
     const int FG = 3;
     const int SAFETY = 2;
+
+    // this block of code takes the user input for the possible scores.
     int score;
     printf("Enter 0 or 1 to STOP\n");
     printf("Enter the NFL score: ");
     scanf("%d", &score);
-    if (score <= 1) {
+    if (score <= 1) { // since there are no possible points worth less than 2, exit
         return 0;
     }
 
     int counts[5] = {0, 0, 0, 0, 0}; // TD_2PCs, TD_1PCs, TDs, FGs, SAFETYs
     printf("Possible combinations of scoring plays if a team's score is 25:\n\n");
+    /*
+     * These nested loops loop through many possible combinations and validates them
+     * during the if statement. Each for loop corresponds to a different source of
+     * score in the NFL (2pc, 1pc, td, fg, safety). The counts[] variable will keep
+     * track of how many of each type of point are currently being assessed, while
+     * the increment variables (i, j, k, l, m) are counted up in the if statement to
+     * check if they all add up to the user-inputted score. If they are, the counts
+     * are printed out in the printf() statement.
+     */
     for (int i = 0; i <= score; i += TD_2PC) { // TD_2PC
         counts[1] = 0;
         for (int j = 0; j <= score; j += TD_1PC) { // TD_1PC
@@ -30,13 +42,13 @@ int main() {
                         }
                         counts[4]++;
                     }
-                counts[3]++;
+                    counts[3]++;
                 }
-            counts[2]++;
+                counts[2]++;
             }
-        counts[1]++;
+            counts[1]++;
         }
-    counts[0]++;
+        counts[0]++;
     }
     return 0;
 }
--- code2.c
+++ code2.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 float celsius_to_fahrenheit(float celsius) {
+    /* takes in a temperature in celsius and converts it to fahrenheit */
     return ((9.0 / 5.0) * celsius) + 32;
 }
 
 float fahrenheit_to_celsius(float fahrenheit) {
+    /* takes in a temperature in fahrenheit and converts it to celsius */
     return (5.0 / 9.0) * (fahrenheit - 32.0);
 }
 
 float celsius_to_kelvin(float celsius) {
+    /* takes in a temperature in celsius and converts it to kelvin */
     float kelvin = celsius + 273.15;
     if (kelvin < 0) {
         kelvin = 0;
@@ -17,10 +20,15 @@ float celsius_to_kelvin(float celsius) {
 }
 
 float kelvin_to_celsius(float kelvin) {
+    /* takes in a temperature in kelvin and converts it to celsius */
     return kelvin - 273.15;
 }
 
 void categorize_temperature(float celsius) {
+    /*
+     * takes in the temperature (in celsius) and prints out an assessment of the
+     * temperature (ex. Freezing) and a weather advisory to go with it. Void.
+     */ 
     printf("Temperature category: ");
     if (celsius < 0.0) {
         printf("Freezing\n");
@@ -41,6 +49,7 @@ void categorize_temperature(float celsius) {
 }
 
 float scale_to_target(float temperature, int scale, int target) {
+    /* takes the three user-inputted variables and runs the conversion logic. */
     float converted;
     switch (scale) {
         case 1:
@@ -79,32 +88,34 @@ int main() {
     float temperature;
     int scale, target;
 
+    // user input
     printf("Enter the temperature: ");
     scanf("%f", &temperature);
-    while (1) {
-        while (1) {
+    while (1) { // loops until valid input
+        while (1) { // loops until valid input
             printf("Choose the current scale (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
             scanf("%d", &scale);
-            if ((scale <= 3) && (scale > 0)) {
+            if ((scale <= 3) && (scale > 0)) { // scale is between 1-3 (inclusive)
                 break;
             }
             printf("Please enter an option 1, 2, or 3!\n");
         }
-        while (1) {
+        while (1) { // loops until valid input
             printf("Convert to (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
             scanf("%d", &target);
-            if ((target <= 3) && (target > 0)) {
+            if ((target <= 3) && (target > 0)) { // scale is between 1-3 (inclusive)
                 break;
             }
             printf("Please enter an option 1, 2, or 3!\n");
         }
-        if (scale != target) {
+        if (scale != target) { // if scale and target are equal, (ex. C -> C) asks again
             break;
         }
         printf("Please choose two unique options for scale and target!\n");
     }
 
     float converted_temperature = scale_to_target(temperature, scale, target);
+    // celsius_temperature is used for categorize_temperature()
     float celsius_temperature = scale_to_target(temperature, scale, 1);
 
     printf("Converted temperature: %f\n", converted_temperature);
